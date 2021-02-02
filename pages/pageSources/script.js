5e3e8a25b6162c9d6c5641a9bc54ef338d36be28
JLUSE63 <68025815+JLUSE63@users.noreply.github.com>
2021-02-02 21:31:58 +0100

    Site
    Mise à jour.

--- pages/pageSources/script.js
+++ pages/pageSources/script.js
@@ -1,69 +1,70 @@
 const Links = [
 	// http://www.rollerenligne.com/articles-473-le-roller-histoire-de-1760-a-nos-jours.html
 	[
-	"1formatik",
-	"https://www.1formatik.com/",
-	"https://www.1formatik.com/4902/comment-creer-un-lien-en-javascript"
+		"1formatik",
+		"https://www.1formatik.com/",
+		"https://www.1formatik.com/4902/comment-creer-un-lien-en-javascript"
 	],
 	[
-	"developer",
-	"https://developer.mozilla.org/",
-	"https://developer.mozilla.org/fr/docs/Web/API"
+		"developer",
+		"https://developer.mozilla.org/",
+		"https://developer.mozilla.org/fr/docs/Web/API"
 	],
 	[
-	"w3docs",
-	"https://fr.w3docs.com/",
-	"https://fr.w3docs.com/snippets/html/comment-creer-un-bouton-html-agissant-comme-un-lien.html"
+		"w3docs",
+		"https://fr.w3docs.com/",
+		"https://fr.w3docs.com/snippets/html/comment-creer-un-bouton-html-agissant-comme-un-lien.html"
 	],
 	[
-	"w3bai",
-	"http://www.w3bai.com/",
-	"http://www.w3bai.com/fr/css/css3_buttons.html"
+		"w3bai",
+		"http://www.w3bai.com/",
+		"http://www.w3bai.com/fr/css/css3_buttons.html"
 	],
 	[
-	"w3bai",
-	"http://www.w3bai.com/",
-	"http://www.w3bai.com/fr/jsref/jsref_abs.html"
+		"w3bai",
+		"http://www.w3bai.com/",
+		"http://www.w3bai.com/fr/jsref/jsref_abs.html"
 	],
 	[
-	"larousse",
-	"https://www.larousse.fr/",
-	"https://www.larousse.fr/dictionnaires/francais/roller/69738"
+		"larousse",
+		"https://www.larousse.fr/",
+		"https://www.larousse.fr/dictionnaires/francais/roller/69738"
 	],
 	[
-	"dictionnaire",
-	"https://dictionnaire.lerobert.com/",
-	"https://dictionnaire.lerobert.com/definition/roller"
+		"dictionnaire",
+		"https://dictionnaire.lerobert.com/",
+		"https://dictionnaire.lerobert.com/definition/roller"
 	],
 	[
-	"wikipedia",
-	"https://fr.wikipedia.org/",
-	"https://fr.wikipedia.org/wiki/Patinage_%C3%A0_roulettes"
+		"wikipedia",
+		"https://fr.wikipedia.org/",
+		"https://fr.wikipedia.org/wiki/Patinage_%C3%A0_roulettes"
 	],
 	[
-	"coolriders",
-	"https://www.coolriders.org/",
-	"https://www.coolriders.org/choisir-ses-rollers/"
+		"coolriders",
+		"https://www.coolriders.org/",
+		"https://www.coolriders.org/choisir-ses-rollers/"
 	],
 	[
-	"htmlcolorcodes",
-	"https://htmlcolorcodes.com/",
-	"https://htmlcolorcodes.com/fr/"
+		"htmlcolorcodes",
+		"https://htmlcolorcodes.com/",
+		"https://htmlcolorcodes.com/fr/"
 	],
 	[
-	"forum",
-	"https://forum.zebulon.fr/",
-	"https://forum.zebulon.fr/topic/187307-r%C3%A9solu-rassembler-plusieurs-pages-html-en-une-seule/"
+		"forum",
+		"https://forum.zebulon.fr/",
+		"https://forum.zebulon.fr/topic/187307-r%C3%A9solu-rassembler-plusieurs-pages-html-en-une-seule/"
 	],
 	[
-	"cascade",
-	"https://la-cascade.io/",
-	"https://la-cascade.io/cacher-des-elements-avec-css/"
+		"cascade",
+		"https://la-cascade.io/",
+		"https://la-cascade.io/cacher-des-elements-avec-css/"
 	],
 	[
-	"w3schools",
-	"https://www.w3schools.com/",
-	"https://www.w3schools.com/jsref/prop_style_backgroundcolor.asp"]
+		"w3schools",
+		"https://www.w3schools.com/",
+		"https://www.w3schools.com/jsref/prop_style_backgroundcolor.asp"
+	]
 ].sort();
 const MyLinks = [
 	[
