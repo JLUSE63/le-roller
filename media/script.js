10a9bb31126f5d88a69a2dbb5e012448c5b75a04
JLUSE63 <68025815+JLUSE63@users.noreply.github.com>
2021-03-11 11:27:19 +0100

    Add files via upload

--- media/script.js
+++ media/script.js
@@ -3,84 +3,78 @@
 
 	function createSources() {
 		const Links = [
-			// http://www.rollerenligne.com/articles-473-le-roller-histoire-de-1760-a-nos-jours.html
 			[
-				"1formatik",
-				"https://www.1formatik.com/",
-				"https://www.1formatik.com/4902/comment-creer-un-lien-en-javascript"
+				"Developpez",
+				"https://www.developpez.net/"
 			],
 			[
-				"developer",
-				"https://developer.mozilla.org/",
-				"https://developer.mozilla.org/fr/docs/Web/API"
+				"GeeksforGeeks",
+				"https://www.geeksforgeeks.org/"
+			],
+			[
+				"L'actualité du roller",
+				"http://www.rollerenligne.com/"
+			],
+			[
+				"1formatik",
+				"https://www.1formatik.com/"
+			],
+			[
+				"developer mozilla (MDN)",
+				"https://developer.mozilla.org/"
 			],
 			[
 				"w3docs",
-				"https://fr.w3docs.com/",
-				"https://fr.w3docs.com/snippets/html/comment-creer-un-bouton-html-agissant-comme-un-lien.html"
+				"https://fr.w3docs.com/"
 			],
 			[
 				"w3bai",
-				"http://www.w3bai.com/",
-				"http://www.w3bai.com/fr/css/css3_buttons.html"
+				"http://www.w3bai.com/"
 			],
 			[
-				"w3bai",
-				"http://www.w3bai.com/",
-				"http://www.w3bai.com/fr/jsref/jsref_abs.html"
-			],
-			[
-				"larousse",
+				"Larousse",
 				"https://www.larousse.fr/",
 				"https://www.larousse.fr/dictionnaires/francais/roller/69738"
 			],
 			[
-				"dictionnaire",
-				"https://dictionnaire.lerobert.com/",
-				"https://dictionnaire.lerobert.com/definition/roller"
+				"LeRobert",
+				"https://dictionnaire.lerobert.com/"
 			],
 			[
 				"wikipedia",
-				"https://fr.wikipedia.org/",
-				"https://fr.wikipedia.org/wiki/Patinage_%C3%A0_roulettes"
+				"https://fr.wikipedia.org/"
 			],
 			[
 				"coolriders",
-				"https://www.coolriders.org/",
-				"https://www.coolriders.org/choisir-ses-rollers/"
+				"https://www.coolriders.org/"
 			],
 			[
 				"htmlcolorcodes",
-				"https://htmlcolorcodes.com/",
-				"https://htmlcolorcodes.com/fr/"
+				"https://htmlcolorcodes.com/"
 			],
 			[
-				"forum",
-				"https://forum.zebulon.fr/",
-				"https://forum.zebulon.fr/topic/187307-r%C3%A9solu-rassembler-plusieurs-pages-html-en-une-seule/"
+				"forum zebulon",
+				"https://forum.zebulon.fr/"
 			],
 			[
-				"cascade",
-				"https://la-cascade.io/",
-				"https://la-cascade.io/cacher-des-elements-avec-css/"
+				"La cascade",
+				"https://la-cascade.io/"
 			],
 			[
 				"w3schools",
-				"https://www.w3schools.com/",
-				"https://www.w3schools.com/jsref/prop_style_backgroundcolor.asp"
+				"https://www.w3schools.com/"
 			],
 			[
-				"colbert-innovation",
-				"https://www.colbert-innovation.eu/",
-				"https://www.colbert-innovation.eu/cahier-histoire/qui-a-invente-le-roulement-a-billes/"
+				"Colbert innovation",
+				"https://www.colbert-innovation.eu/"
 			]
 		].sort();
 		for (i in Links) {
 			let a = document.createElement('a');
 		    let li = document.createElement('li');
-		    let lien = document.createTextNode(Links[i][1]);
+		    let lien = document.createTextNode(Links[i][0]);
 		    a.appendChild(lien);
-		    a.href = Links[i][2];
+		    a.href = Links[i][1];
 		    li.appendChild(a);
 		    document.getElementById("NosSources").appendChild(li);
 	    };
